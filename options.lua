5d40719b5f8017bd02b7ad01fd6880f0a0c1d55e
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2025-03-19 16:45:50 +0300

    fix guicursor

--- options.lua
+++ options.lua
@@ -26,4 +26,4 @@ vim.schedule(function()
 end)
 
 -- Set cursor shapes
-vim.opt.guicursor="n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50 ,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+vim.opt.guicursor="n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
